f74cf3db9c58cd67486cf9d3e89a3752f775895c
maojie <41260899+aaaaaaMao@users.noreply.github.com>
2022-06-26 23:41:53 +0800

    feat: Update options page
    Add ColorButton component

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,10 +1,23 @@
 <template>
     <div id="buttonDiv">
+    <ColorButton 
+      v-for="buttonColor in presetButtonColors"
+      :buttonColor="buttonColor"
+      ></ColorButton>
     </div>
     <div>
       <p>Choose a different background color!</p>
     </div>
 </template>
+
+<script setup lang="ts">
+import ColorButton from './components/ColorButton.vue';
+const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"]
+</script>
+
+<script lang="ts">
+export default {}
+</script>
 
 <style>
 </style>
---
+++ src/options/components/ColorButton.vue
@@ -0,0 +1,18 @@
+<template>
+<button 
+  :dataset="{color: buttonColor}"
+  :style="{backgroundColor: buttonColor}"
+  @click="handleButtonClick"
+  ></button>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['buttonColor'],
+  methods: {
+    handleButtonClick() {
+      chrome.storage.sync.set({ color: (this as any).buttonColor });
+    }
+  }
+}
+</script>
